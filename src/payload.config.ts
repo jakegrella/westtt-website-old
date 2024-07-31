d24a56dae9c7d36b874b813883b375bf8e75cb82
jake grella <jake@jakegrella.com>
2024-07-30 20:25:44 -0700

    chore: DATABSE_URI -> POSTGRES_URL

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -33,7 +33,7 @@ export default buildConfig({
   },
   db: postgresAdapter({
     pool: {
-      connectionString: process.env.DATABASE_URI || '',
+      connectionString: process.env.POSTGRES_URL || '',
     },
   }),
   sharp,
