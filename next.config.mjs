0cc87d396f946757bf35d123a876ca3f68d39c03
jake grella <jake@jakegrella.com>
2024-08-04 08:48:10 -0700

    chore: test remove remote pattern

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,16 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'westtt.com',
-        port: '',
-        pathname: '/**'
-      },
-    ],
-  },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'westtt.com',
+  //       port: '',
+  //       pathname: '/**'
+  //     },
+  //   ],
+  // },
 }
 
 export default withPayload(nextConfig)
